a2402a8de81847e01481432b0a4958feff3f88c4
david_dd <demuriya.ds@phystech.ru>
2022-09-22 07:10:27 +0300

    error handler added

--- src/handlers/recieve_image.cpp
+++ src/handlers/recieve_image.cpp
@@ -13,27 +13,29 @@ using namespace cimg_library;
 
 
 void imgRequestHandler::handleRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response){
-    response.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
-	response.setContentType("img/jpg");
+	response.setContentType("image/jpeg");
+    unsigned size = request.getContentLength();
 
-	std::ostream& response_img_stream = response.send();
+	std::ostream &response_img_stream = response.send();
 
     std::istream &request_img_stream = request.stream();
-    unsigned size = request.getContentLength();
     
-    unsigned char* buffer = new unsigned char[size];
     JOCTET *buffer_input = new JOCTET[size];
-
-    //std::FILE *desc = std::fmemopen(buffer, size, "rb+");
-    
-    //if (!std::fread(buffer_input, sizeof(JOCTET), size, desc)) 
-    //    std::cout << "Cannot read an image" << std::endl;
 
     request_img_stream.read(reinterpret_cast<char*>(buffer_input), size);
     CImg<unsigned char> img;
-    img.load_jpeg_buffer(buffer_input, size);
+    
+    if(!img.load_jpeg_buffer(buffer_input, size)){
+        std::ostream &out = response.send();
+        out << "Failed to decode jpeg image.";
+        out.flush();
+        response.setStatus(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
+    }
+
     img.mirror("x");
     JOCTET *buffer_output = new JOCTET[size];
     img.save_jpeg_buffer(buffer_output,size,60); 
+
     response_img_stream.write(reinterpret_cast<char*>(buffer_output), size);
+    response.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
 }
